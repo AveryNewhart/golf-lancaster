52c9bd09442b155bb365bfe1910051f9946b01c0
AveryNewhart <newhartreeceavery@gmail.com>
2023-08-16 20:56:38 -0400

    header file created and imported

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
+  import Nav from './components/Nav.vue';
 </script>
 
 <template>
+  <Nav />
   <div>
     <h1>Golf Lancaster</h1>
   </div>
 </template>
 
 <style scoped>
+
 </style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    header goes here
+</template>
+  
+  <style scoped>
+  
+  </style>
