f91db7429f03af32c209f356c53b486ef8b1c8be
AveryNewhart <newhartreeceavery@gmail.com>
2023-08-17 20:40:52 -0400

    nav added, grid created, ready for research

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,20 @@
 <template>
   <Nav />
   <div>
-    <h1>Golf Lancaster</h1>
+    <h1 class="text-center mt-8">Courses In Lancaster County</h1>
+  </div>
+  <div class="grid grid-cols-4 gap-4 text-center mt-8">
+    <div>Each course Div</div>
+    <div>Each course Div</div>
+    <div>Each course Div</div>
+    <div>Each course Div</div>
+    <div>Each course Div</div>
+    <div>Each course Div</div>
+    <div>Each course Div</div>
+    <div>Each course Div</div>
+    <div>Each course Div</div>
+    <div>Each course Div</div>
+    <div>Each course Div</div>
   </div>
 </template>
 
