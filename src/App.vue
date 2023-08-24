2029a23ccf2ab9606f11d2c7457b9bd47145cef3
AveryNewhart <newhartreeceavery@gmail.com>
2023-08-24 19:02:40 -0400

    general format/layout

--- src/App.vue
+++ src/App.vue
@@ -5,20 +5,105 @@
 <template>
   <Nav />
   <div>
-    <h1 class="text-center mt-8">Courses In Lancaster County</h1>
+    <h1 class="text-center mt-8 text-white text-3xl font-semibold">Courses In Lancaster County</h1>
   </div>
   <div class="grid grid-cols-4 gap-4 text-center mt-8">
-    <div>Each course Div</div>
-    <div>Each course Div</div>
-    <div>Each course Div</div>
-    <div>Each course Div</div>
-    <div>Each course Div</div>
-    <div>Each course Div</div>
-    <div>Each course Div</div>
-    <div>Each course Div</div>
-    <div>Each course Div</div>
-    <div>Each course Div</div>
-    <div>Each course Div</div>
+    <div>
+      <p>img here</p>
+      <p>course name</p>
+      <p>rates for 9</p>
+      <p>rates for 18</p>
+      <!-- make a clickable link for the a/p tag -->
+      <a><p>more course info</p></a>
+    </div>
+    <div>
+      <p>img here</p>
+      <p>course name</p>
+      <p>rates for 9</p>
+      <p>rates for 18</p>
+      <!-- make a clickable link for the a/p tag -->
+      <a><p>more course info</p></a>
+    </div>
+    <div>
+      <p>img here</p>
+      <p>course name</p>
+      <p>rates for 9</p>
+      <p>rates for 18</p>
+      <!-- make a clickable link for the a/p tag -->
+      <a><p>more course info</p></a>
+    </div>
+    <div>
+      <p>img here</p>
+      <p>course name</p>
+      <p>rates for 9</p>
+      <p>rates for 18</p>
+      <!-- make a clickable link for the a/p tag -->
+      <a><p>more course info</p></a>
+    </div>
+    <div>
+      <p>img here</p>
+      <p>course name</p>
+      <p>rates for 9</p>
+      <p>rates for 18</p>
+      <!-- make a clickable link for the a/p tag -->
+      <a><p>more course info</p></a>
+    </div>
+    <div>
+      <p>img here</p>
+      <p>course name</p>
+      <p>rates for 9</p>
+      <p>rates for 18</p>
+      <!-- make a clickable link for the a/p tag -->
+      <a><p>more course info</p></a>
+    </div>
+    <div>
+      <p>img here</p>
+      <p>course name</p>
+      <p>rates for 9</p>
+      <p>rates for 18</p>
+      <!-- make a clickable link for the a/p tag -->
+      <a><p>more course info</p></a>
+    </div>
+    <div>
+      <p>img here</p>
+      <p>course name</p>
+      <p>rates for 9</p>
+      <p>rates for 18</p>
+      <!-- make a clickable link for the a/p tag -->
+      <a><p>more course info</p></a>
+    </div>
+    <div>
+      <p>img here</p>
+      <p>course name</p>
+      <p>rates for 9</p>
+      <p>rates for 18</p>
+      <!-- make a clickable link for the a/p tag -->
+      <a><p>more course info</p></a>
+    </div>
+    <div>
+      <p>img here</p>
+      <p>course name</p>
+      <p>rates for 9</p>
+      <p>rates for 18</p>
+      <!-- make a clickable link for the a/p tag -->
+      <a><p>more course info</p></a>
+    </div>
+    <div>
+      <p>img here</p>
+      <p>course name</p>
+      <p>rates for 9</p>
+      <p>rates for 18</p>
+      <!-- make a clickable link for the a/p tag -->
+      <a><p>more course info</p></a>
+    </div>
+    <div>
+      <p>img here</p>
+      <p>course name</p>
+      <p>rates for 9</p>
+      <p>rates for 18</p>
+      <!-- make a clickable link for the a/p tag -->
+      <a><p>more course info</p></a>
+    </div>
   </div>
 </template>
 
