9a4ed022493cc5d20344b2a1ed0175b62cb76338
AveryNewhart <newhartreeceavery@gmail.com>
2023-08-24 21:22:10 -0400

    first photo added and layout for adding new courses added, ready to add all info for courses

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,27 @@
 <script setup lang="ts">
   import Nav from './components/Nav.vue';
+
+  interface Course {
+  name: string;
+  image: string;
+  rating: number;
+  address: string;
+  rates9: string;
+  rates18: string;
+}
+
+const courses: Course[] = [
+  {
+    name: 'Four Seasons Golf Club',
+    image: './src/assets/imgs/fourseasons.webp', // Adjust path as needed
+    rating: 4.5,
+    address: '123 Golf Course Rd, Lancaster, PA',
+    rates9: '$30',
+    rates18: '$50',
+  },
+  // Add other courses here
+];
+
 </script>
 
 <template>
@@ -8,15 +30,15 @@
     <h1 class="text-center mt-8 text-white text-3xl font-semibold">Courses In Lancaster County</h1>
   </div>
   <div class="grid grid-cols-4 gap-4 text-center mt-8">
-    <div>
-      <p>img here</p>
-      <p>Four Seasons Golf Club</p>
-      <p>rating</p>
-      <p>address</p>
-      <p>rates for 9</p>
-      <p>rates for 18</p>
-      <!-- make a clickable link for the a/p tag -->
-      <a><p>more course info</p></a>
+ <!-- Repeat this block for each golf course -->
+    <div v-for="course in courses" :key="course.name">
+      <img :src="course.image" alt="Course Image" class="w-full h-auto" />
+      <p>{{ course.name }}</p>
+      <p>Rating: {{ course.rating }}</p>
+      <p>Address: {{ course.address }}</p>
+      <p>Rates for 9: {{ course.rates9 }}</p>
+      <p>Rates for 18: {{ course.rates18 }}</p>
+      <a href="#">More course info</a>
     </div>
     <div>
       <p>img here</p>
