f1fd53a549778a5fdcd418f17efa3fb4855e1240
AveryNewhart <newhartreeceavery@gmail.com>
2023-08-26 13:17:08 -0400

    all courses names and photos added

--- src/App.vue
+++ src/App.vue
@@ -13,13 +13,164 @@
 const courses: Course[] = [
   {
     name: 'Four Seasons Golf Club',
-    image: './src/assets/imgs/fourseasons.webp', // Adjust path as needed
+    image: './src/assets/imgs/fourseasons.webp',
     rating: 4.5,
     address: '123 Golf Course Rd, Lancaster, PA',
     rates9: '$30',
     rates18: '$50',
   },
-  // Add other courses here
+  {
+    name: 'Foxchase Golf Glub',
+    image: './src/assets/imgs/foxchase.png',
+    rating: 4.5,
+    address: '123 Golf Course Rd, Lancaster, PA',
+    rates9: '$30',
+    rates18: '$50',
+  },
+  {
+    name: 'Overlook Golf Course',
+    image: './src/assets/imgs/overlook.jpeg',
+    rating: 4.5,
+    address: '123 Golf Course Rd, Lancaster, PA',
+    rates9: '$30',
+    rates18: '$50',
+  },
+  {
+    name: 'Pilgrims Oak Golf Course',
+    image: './src/assets/imgs/pilgrimsoaks.jpeg',
+    rating: 4.5,
+    address: '123 Golf Course Rd, Lancaster, PA',
+    rates9: '$30',
+    rates18: '$50',
+  },
+  {
+    name: 'Highlands of Donegal',
+    image: './src/assets/imgs/highlands.png',
+    rating: 4.5,
+    address: '123 Golf Course Rd, Lancaster, PA',
+    rates9: '$30',
+    rates18: '$50',
+  },
+  {
+    name: 'Tanglewood Manor Golf Club',
+    image: './src/assets/imgs/tanglewood.png',
+    rating: 4.5,
+    address: '123 Golf Course Rd, Lancaster, PA',
+    rates9: '$30',
+    rates18: '$50',
+  },
+  {
+    name: 'Evergreen Golf Course',
+    image: './src/assets/imgs/evergreen.jpeg',
+    rating: 4.5,
+    address: '123 Golf Course Rd, Lancaster, PA',
+    rates9: '$30',
+    rates18: '$50',
+  },
+  {
+    name: 'Treetop Golf Course',
+    image: './src/assets/imgs/treetop.jpeg',
+    rating: 4.5,
+    address: '123 Golf Course Rd, Lancaster, PA',
+    rates9: '$30',
+    rates18: '$50',
+  },
+  {
+    name: 'Crossgates Golf Club',
+    image: './src/assets/imgs/crossgates.png',
+    rating: 4.5,
+    address: '123 Golf Course Rd, Lancaster, PA',
+    rates9: '$30',
+    rates18: '$50',
+  },
+  {
+    name: 'Meadia Heights Golf Club',
+    image: './src/assets/imgs/meadia.jpeg',
+    rating: 4.5,
+    address: '123 Golf Course Rd, Lancaster, PA',
+    rates9: '$30',
+    rates18: '$50',
+  },
+  {
+    name: 'Willow Valley Golf Course',
+    image: './src/assets/imgs/willow.jpeg',
+    rating: 4.5,
+    address: '123 Golf Course Rd, Lancaster, PA',
+    rates9: '$30',
+    rates18: '$50',
+  },
+  {
+    name: 'Lancaster Country Club',
+    image: './src/assets/imgs/lancastercountry.jpeg',
+    rating: 4.5,
+    address: '123 Golf Course Rd, Lancaster, PA',
+    rates9: '$30',
+    rates18: '$50',
+  },
+  {
+    name: 'Springside Par 3 Golf Course',
+    image: './src/assets/imgs/springside.jpeg',
+    rating: 4.5,
+    address: '123 Golf Course Rd, Lancaster, PA',
+    rates9: '$30',
+    rates18: '$50',
+  },
+  {
+    name: 'Iron Valley Golf Club',
+    image: './src/assets/imgs/ironvalley.png',
+    rating: 4.5,
+    address: '123 Golf Course Rd, Lancaster, PA',
+    rates9: '$30',
+    rates18: '$50',
+  },
+  {
+    name: 'Heritage Hills Golf Resort',
+    image: './src/assets/imgs/heritagehills.jpeg',
+    rating: 4.5,
+    address: '123 Golf Course Rd, Lancaster, PA',
+    rates9: '$30',
+    rates18: '$50',
+  },
+  {
+    name: 'Royal Oaks Golf Club',
+    image: './src/assets/imgs/royaloaks.jpeg',
+    rating: 4.5,
+    address: '123 Golf Course Rd, Lancaster, PA',
+    rates9: '$30',
+    rates18: '$50',
+  },
+  {
+    name: 'Moccasin Run Golf Club',
+    image: './src/assets/imgs/moccasin.jpeg',
+    rating: 4.5,
+    address: '123 Golf Course Rd, Lancaster, PA',
+    rates9: '$30',
+    rates18: '$50',
+  },
+  {
+    name: 'Galen Hall Golf Course',
+    image: './src/assets/imgs/galen.jpeg',
+    rating: 4.5,
+    address: '123 Golf Course Rd, Lancaster, PA',
+    rates9: '$30',
+    rates18: '$50',
+  },
+  {
+    name: 'Par Line Golf Course',
+    image: './src/assets/imgs/parline.jpeg',
+    rating: 4.5,
+    address: '123 Golf Course Rd, Lancaster, PA',
+    rates9: '$30',
+    rates18: '$50',
+  },
+  {
+    name: 'Chapel Hill Golf Course',
+    image: './src/assets/imgs/chapelhill.png',
+    rating: 4.5,
+    address: '123 Golf Course Rd, Lancaster, PA',
+    rates9: '$30',
+    rates18: '$50',
+  },
 ];
 
 </script>
@@ -30,7 +181,6 @@ const courses: Course[] = [
     <h1 class="text-center mt-8 text-white text-3xl font-semibold">Courses In Lancaster County</h1>
   </div>
   <div class="grid grid-cols-4 gap-4 text-center mt-8">
- <!-- Repeat this block for each golf course -->
     <div v-for="course in courses" :key="course.name">
       <img :src="course.image" alt="Course Image" class="w-full h-auto" />
       <p>{{ course.name }}</p>
@@ -40,199 +190,13 @@ const courses: Course[] = [
       <p>Rates for 18: {{ course.rates18 }}</p>
       <a href="#">More course info</a>
     </div>
-    <div>
-      <p>img here</p>
-      <p>Foxchase Golf Club</p>
-      <p>rating</p>
-      <p>address</p>
-      <p>rates for 9</p>
-      <p>rates for 18</p>
-      <!-- make a clickable link for the a/p tag -->
-      <a><p>more course info</p></a>
-    </div>
-    <div>
-      <p>img here</p>
-      <p>Overlook Golf Course</p>
-      <p>rating</p>
-      <p>address</p>
-      <p>rates for 9</p>
-      <p>rates for 18</p>
-      <!-- make a clickable link for the a/p tag -->
-      <a><p>more course info</p></a>
-    </div>
-    <div>
-      <p>img here</p>
-      <p>Pilgrim's Oak Golf Course</p>
-      <p>rating</p>
-      <p>address</p>
-      <p>rates for 9</p>
-      <p>rates for 18</p>
-      <!-- make a clickable link for the a/p tag -->
-      <a><p>more course info</p></a>
-    </div>
-    <div>
-      <p>img here</p>
-      <p>Highlands of Donegal</p>
-      <p>rating</p>
-      <p>address</p>
-      <p>rates for 9</p>
-      <p>rates for 18</p>
-      <!-- make a clickable link for the a/p tag -->
-      <a><p>more course info</p></a>
-    </div>
-    <div>
-      <p>img here</p>
-      <p>Tanglewood Manor Golf Club</p>
-      <p>rating</p>
-      <p>address</p>
-      <p>rates for 9</p>
-      <p>rates for 18</p>
-      <!-- make a clickable link for the a/p tag -->
-      <a><p>more course info</p></a>
-    </div>
-    <div>
-      <p>img here</p>
-      <p>Evergreen Golf Course</p>
-      <p>rating</p>
-      <p>address</p>
-      <p>rates for 9</p>
-      <p>rates for 18</p>
-      <!-- make a clickable link for the a/p tag -->
-      <a><p>more course info</p></a>
-    </div>
-    <div>
-      <p>img here</p>
-      <p>Tree Top Golf Course</p>
-      <p>rating</p>
-      <p>address</p>
-      <p>rates for 9</p>
-      <p>rates for 18</p>
-      <!-- make a clickable link for the a/p tag -->
-      <a><p>more course info</p></a>
-    </div>
-    <div>
-      <p>img here</p>
-      <p>Crossgates Golf Club</p>
-      <p>rating</p>
-      <p>address</p>
-      <p>rates for 9</p>
-      <p>rates for 18</p>
-      <!-- make a clickable link for the a/p tag -->
-      <a><p>more course info</p></a>
-    </div>
-    <div>
-      <p>img here</p>
-      <p>Meadia Heights Golf Club</p>
-      <p>rating</p>
-      <p>address</p>
-      <p>rates for 9</p>
-      <p>rates for 18</p>
-      <!-- make a clickable link for the a/p tag -->
-      <a><p>more course info</p></a>
-    </div>
-    <div>
-      <p>img here</p>
-      <p>Willow Valley Golf Course</p>
-      <p>rating</p>
-      <p>address</p>
-      <p>rates for 9</p>
-      <p>rates for 18</p>
-      <!-- make a clickable link for the a/p tag -->
-      <a><p>more course info</p></a>
-    </div>
-    <div>
-      <p>img here</p>
-      <p>Lancaster Country Club</p>
-      <p>rating</p>
-      <p>address</p>
-      <p>rates for 9</p>
-      <p>rates for 18</p>
-      <!-- make a clickable link for the a/p tag -->
-      <a><p>more course info</p></a>
-    </div>
-    <div>
-      <p>img here</p>
-      <p>Springside Par 3 Golf Course</p>
-      <p>rating</p>
-      <p>address</p>
-      <p>rates for 9</p>
-      <p>rates for 18</p>
-      <!-- make a clickable link for the a/p tag -->
-      <a><p>more course info</p></a>
-    </div>
-    <div>
-      <p>img here</p>
-      <p>Iron Valley Golf Club</p>
-      <p>rating</p>
-      <p>address</p>
-      <p>rates for 9</p>
-      <p>rates for 18</p>
-      <!-- make a clickable link for the a/p tag -->
-      <a><p>more course info</p></a>
-    </div>
-    <div>
-      <p>img here</p>
-      <p>Heritage Hills Golf Resort</p>
-      <p>rating</p>
-      <p>address</p>
-      <p>rates for 9</p>
-      <p>rates for 18</p>
-      <!-- make a clickable link for the a/p tag -->
-      <a><p>more course info</p></a>
-    </div>
-    <div>
-      <p>img here</p>
-      <p>Royal Oaks Golf Club</p>
-      <p>rating</p>
-      <p>address</p>
-      <p>rates for 9</p>
-      <p>rates for 18</p>
-      <!-- make a clickable link for the a/p tag -->
-      <a><p>more course info</p></a>
-    </div>
-    <div>
-      <p>img here</p>
-      <p>Moccasin Run Golf Club</p>
-      <p>rating</p>
-      <p>address</p>
-      <p>rates for 9</p>
-      <p>rates for 18</p>
-      <!-- make a clickable link for the a/p tag -->
-      <a><p>more course info</p></a>
-    </div>
-    <div>
-      <p>img here</p>
-      <p>Galen Hall Golf Course</p>
-      <p>rating</p>
-      <p>address</p>
-      <p>rates for 9</p>
-      <p>rates for 18</p>
-      <!-- make a clickable link for the a/p tag -->
-      <a><p>more course info</p></a>
-    </div>
-    <div>
-      <p>img here</p>
-      <p>Par Line Golf Course</p>
-      <p>rating</p>
-      <p>address</p>
-      <p>rates for 9</p>
-      <p>rates for 18</p>
-      <!-- make a clickable link for the a/p tag -->
-      <a><p>more course info</p></a>
-    </div>
-    <div>
-      <p>img here</p>
-      <p>Chapel Hill Golf Course</p>
-      <p>rating</p>
-      <p>address</p>
-      <p>rates for 9</p>
-      <p>rates for 18</p>
-      <!-- make a clickable link for the a/p tag -->
-      <a><p>more course info</p></a>
-    </div>
   </div>
 </template>
 
 <style scoped>
+
+img {
+  height: 250px;
+}
 
 </style>
