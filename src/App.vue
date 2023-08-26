acfa7d7940b99a21e14782180d3dc2b47de4c1e9
AveryNewhart <newhartreeceavery@gmail.com>
2023-08-26 14:15:48 -0400

    rating changed, note added, and first course with all info added

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
   interface Course {
   name: string;
   image: string;
-  rating: number;
+  rating: string;
   address: string;
   rates9weekday: string;
   rates9weekend: string;
@@ -13,26 +13,28 @@
   rates18weekend: string;
   rates18junior: string;
   rates18senior: string;
+  notes: string;
 }
 
 const courses: Course[] = [
   {
     name: 'Four Seasons Golf Club',
     image: './src/assets/imgs/fourseasons.webp',
-    rating: 4.5,
-    address: '123 Golf Course Rd, Lancaster, PA',
-    rates9weekday: '$30',
-    rates9weekend: '$30',
-    rates9senior: '$30',
-    rates18weekday: '$30',
-    rates18weekend: '$30',
-    rates18junior: '$30',
-    rates18senior: '$50',
+    rating: '4.3/5',
+    address: '949 Church Street Landisville, PA 17538',
+    rates9weekday: '$20(walking) | $34(cart)',
+    rates9weekend: '$22(walking) | $36(cart)',
+    rates9senior: '$2 Off',
+    rates18weekday: '$31(walking) | $45(cart)',
+    rates18weekend: '$43(walking) | $57(cart)',
+    rates18junior: '$11 Off(weekday) and $17 Off(weekend)',
+    rates18senior: '$8 Off(weekday) and $11 Off(weekend)',
+    notes: 'Check course site for the specific rates for 18 holes for the different times of the day.',
   },
   {
     name: 'Foxchase Golf Glub',
     image: './src/assets/imgs/foxchase.png',
-    rating: 4.5,
+    rating: '4.3/5',
     address: '123 Golf Course Rd, Lancaster, PA',
     rates9weekday: '$30',
     rates9weekend: '$30',
@@ -41,11 +43,12 @@ const courses: Course[] = [
     rates18weekend: '$30',
     rates18junior: '$30',
     rates18senior: '$50',
+    notes: 'put notes here about rates, like if time of day changes rates',
   },
   {
     name: 'Overlook Golf Course',
     image: './src/assets/imgs/overlook.jpeg',
-    rating: 4.5,
+    rating: '4.3/5',
     address: '123 Golf Course Rd, Lancaster, PA',
     rates9weekday: '$30',
     rates9weekend: '$30',
@@ -54,11 +57,12 @@ const courses: Course[] = [
     rates18weekend: '$30',
     rates18junior: '$30',
     rates18senior: '$50',
+    notes: 'put notes here about rates, like if time of day changes rates',
   },
   {
     name: 'Pilgrims Oak Golf Course',
     image: './src/assets/imgs/pilgrimsoaks.jpeg',
-    rating: 4.5,
+    rating: '4.3/5',
     address: '123 Golf Course Rd, Lancaster, PA',
     rates9weekday: '$30',
     rates9weekend: '$30',
@@ -67,11 +71,12 @@ const courses: Course[] = [
     rates18weekend: '$30',
     rates18junior: '$30',
     rates18senior: '$50',
+    notes: 'put notes here about rates, like if time of day changes rates',
   },
   {
     name: 'Highlands of Donegal',
     image: './src/assets/imgs/highlands.png',
-    rating: 4.5,
+    rating: '4.3/5',
     address: '123 Golf Course Rd, Lancaster, PA',
     rates9weekday: '$30',
     rates9weekend: '$30',
@@ -80,11 +85,12 @@ const courses: Course[] = [
     rates18weekend: '$30',
     rates18junior: '$30',
     rates18senior: '$50',
+    notes: 'put notes here about rates, like if time of day changes rates',
   },
   {
     name: 'Tanglewood Manor Golf Club',
     image: './src/assets/imgs/tanglewood.png',
-    rating: 4.5,
+    rating: '4.3/5',
     address: '123 Golf Course Rd, Lancaster, PA',
     rates9weekday: '$30',
     rates9weekend: '$30',
@@ -93,11 +99,12 @@ const courses: Course[] = [
     rates18weekend: '$30',
     rates18junior: '$30',
     rates18senior: '$50',
+    notes: 'put notes here about rates, like if time of day changes rates',
   },
   {
     name: 'Evergreen Golf Course',
     image: './src/assets/imgs/evergreen.jpeg',
-    rating: 4.5,
+    rating: '4.3/5',
     address: '123 Golf Course Rd, Lancaster, PA',
     rates9weekday: '$30',
     rates9weekend: '$30',
@@ -106,11 +113,12 @@ const courses: Course[] = [
     rates18weekend: '$30',
     rates18junior: '$30',
     rates18senior: '$50',
+    notes: 'put notes here about rates, like if time of day changes rates',
   },
   {
     name: 'Treetop Golf Course',
     image: './src/assets/imgs/treetop.jpeg',
-    rating: 4.5,
+    rating: '4.3/5',
     address: '123 Golf Course Rd, Lancaster, PA',
     rates9weekday: '$30',
     rates9weekend: '$30',
@@ -119,11 +127,12 @@ const courses: Course[] = [
     rates18weekend: '$30',
     rates18junior: '$30',
     rates18senior: '$50',
+    notes: 'put notes here about rates, like if time of day changes rates',
   },
   {
     name: 'Crossgates Golf Club',
     image: './src/assets/imgs/crossgates.png',
-    rating: 4.5,
+    rating: '4.3/5',
     address: '123 Golf Course Rd, Lancaster, PA',
     rates9weekday: '$30',
     rates9weekend: '$30',
@@ -132,11 +141,12 @@ const courses: Course[] = [
     rates18weekend: '$30',
     rates18junior: '$30',
     rates18senior: '$50',
+    notes: 'put notes here about rates, like if time of day changes rates',
   },
   {
     name: 'Meadia Heights Golf Club',
     image: './src/assets/imgs/meadia.jpeg',
-    rating: 4.5,
+    rating: '4.3/5',
     address: '123 Golf Course Rd, Lancaster, PA',
     rates9weekday: '$30',
     rates9weekend: '$30',
@@ -145,11 +155,12 @@ const courses: Course[] = [
     rates18weekend: '$30',
     rates18junior: '$30',
     rates18senior: '$50',
+    notes: 'put notes here about rates, like if time of day changes rates',
   },
   {
     name: 'Willow Valley Golf Course',
     image: './src/assets/imgs/willow.jpeg',
-    rating: 4.5,
+    rating: '4.3/5',
     address: '123 Golf Course Rd, Lancaster, PA',
     rates9weekday: '$30',
     rates9weekend: '$30',
@@ -158,11 +169,12 @@ const courses: Course[] = [
     rates18weekend: '$30',
     rates18junior: '$30',
     rates18senior: '$50',
+    notes: 'put notes here about rates, like if time of day changes rates',
   },
   {
     name: 'Lancaster Country Club',
     image: './src/assets/imgs/lancastercountry.jpeg',
-    rating: 4.5,
+    rating: '4.3/5',
     address: '123 Golf Course Rd, Lancaster, PA',
     rates9weekday: '$30',
     rates9weekend: '$30',
@@ -171,11 +183,12 @@ const courses: Course[] = [
     rates18weekend: '$30',
     rates18junior: '$30',
     rates18senior: '$50',
+    notes: 'put notes here about rates, like if time of day changes rates',
   },
   {
     name: 'Springside Par 3 Golf Course',
     image: './src/assets/imgs/springside.jpeg',
-    rating: 4.5,
+    rating: '4.3/5',
     address: '123 Golf Course Rd, Lancaster, PA',
     rates9weekday: '$30',
     rates9weekend: '$30',
@@ -184,11 +197,12 @@ const courses: Course[] = [
     rates18weekend: '$30',
     rates18junior: '$30',
     rates18senior: '$50',
+    notes: 'put notes here about rates, like if time of day changes rates',
   },
   {
     name: 'Iron Valley Golf Club',
     image: './src/assets/imgs/ironvalley.png',
-    rating: 4.5,
+    rating: '4.3/5',
     address: '123 Golf Course Rd, Lancaster, PA',
     rates9weekday: '$30',
     rates9weekend: '$30',
@@ -197,11 +211,12 @@ const courses: Course[] = [
     rates18weekend: '$30',
     rates18junior: '$30',
     rates18senior: '$50',
+    notes: 'put notes here about rates, like if time of day changes rates',
   },
   {
     name: 'Heritage Hills Golf Resort',
     image: './src/assets/imgs/heritagehills.jpeg',
-    rating: 4.5,
+    rating: '4.3/5',
     address: '123 Golf Course Rd, Lancaster, PA',
     rates9weekday: '$30',
     rates9weekend: '$30',
@@ -210,11 +225,12 @@ const courses: Course[] = [
     rates18weekend: '$30',
     rates18junior: '$30',
     rates18senior: '$50',
+    notes: 'put notes here about rates, like if time of day changes rates',
   },
   {
     name: 'Royal Oaks Golf Club',
     image: './src/assets/imgs/royaloaks.jpeg',
-    rating: 4.5,
+    rating: '4.3/5',
     address: '123 Golf Course Rd, Lancaster, PA',
     rates9weekday: '$30',
     rates9weekend: '$30',
@@ -223,11 +239,12 @@ const courses: Course[] = [
     rates18weekend: '$30',
     rates18junior: '$30',
     rates18senior: '$50',
+    notes: 'put notes here about rates, like if time of day changes rates',
   },
   {
     name: 'Moccasin Run Golf Club',
     image: './src/assets/imgs/moccasin.jpeg',
-    rating: 4.5,
+    rating: '4.3/5',
     address: '123 Golf Course Rd, Lancaster, PA',
     rates9weekday: '$30',
     rates9weekend: '$30',
@@ -236,11 +253,12 @@ const courses: Course[] = [
     rates18weekend: '$30',
     rates18junior: '$30',
     rates18senior: '$50',
+    notes: 'put notes here about rates, like if time of day changes rates',
   },
   {
     name: 'Galen Hall Golf Course',
     image: './src/assets/imgs/galen.jpeg',
-    rating: 4.5,
+    rating: '4.3/5',
     address: '123 Golf Course Rd, Lancaster, PA',
     rates9weekday: '$30',
     rates9weekend: '$30',
@@ -249,11 +267,12 @@ const courses: Course[] = [
     rates18weekend: '$30',
     rates18junior: '$30',
     rates18senior: '$50',
+    notes: 'put notes here about rates, like if time of day changes rates',
   },
   {
     name: 'Par Line Golf Course',
     image: './src/assets/imgs/parline.jpeg',
-    rating: 4.5,
+    rating: '4.3/5',
     address: '123 Golf Course Rd, Lancaster, PA',
     rates9weekday: '$30',
     rates9weekend: '$30',
@@ -262,11 +281,12 @@ const courses: Course[] = [
     rates18weekend: '$30',
     rates18junior: '$30',
     rates18senior: '$50',
+    notes: 'put notes here about rates, like if time of day changes rates',
   },
   {
     name: 'Chapel Hill Golf Course',
     image: './src/assets/imgs/chapelhill.png',
-    rating: 4.5,
+    rating: '4.3/5',
     address: '123 Golf Course Rd, Lancaster, PA',
     rates9weekday: '$30',
     rates9weekend: '$30',
@@ -275,6 +295,7 @@ const courses: Course[] = [
     rates18weekend: '$30',
     rates18junior: '$30',
     rates18senior: '$50',
+    notes: 'put notes here about rates, like if time of day changes rates',
   },
 ];
 
@@ -298,6 +319,7 @@ const courses: Course[] = [
       <p>Rates for 18(Weekend): {{ course.rates18weekend }}</p>
       <p>Rates for 18(Junior): {{ course.rates18junior }}</p>
       <p>Rates for 18(Senior): {{ course.rates18senior }}</p>
+      <p>Notes: {{ course.notes }}</p>
       <a href="#">More course info</a>
     </div>
   </div>
