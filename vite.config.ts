4e5de3106fbde3d709ba089ec62bd9ea18d2427f
AveryNewhart <newhartreeceavery@gmail.com>
2023-08-27 18:42:00 -0400

    new config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/golf-lancaster/',
   plugins: [vue()],
-})
+  build: {
+    assetsDir: 'assets', // This specifies the directory for assets (like images)
+    // ... other build options
+  },
+});
+
